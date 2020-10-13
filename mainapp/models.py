c10be854a15a237224f13b528f11387db084a8a1
igorworkpc <sonikry.99@gmail.com>
2020-10-13 16:09:55 +0500

    fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Pictures(models.Model):
+    name = models.CharField("Name of picture", max_length=50)
+    date_upload = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField("Picture", upload_to="images/pictures", default="", blank=True)
+
+    class Meta:
+        verbose_name = "picture"
+        verbose_name_plural = "pictures"
+        ordering = ["name"]
+
+
